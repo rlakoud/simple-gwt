722d7659891996d7c0ac5148a838c028b6eaab66
itruett <itruett@1d095b2c-cabc-11dd-87b6-9d1a99559f42>
2008-12-16 23:07:10 +0000

    Initialization API.

---
+++ src/core/com/googlecode/simplegwt/client/ui/initialization/InitializationEvent.java
@@ -0,0 +1,50 @@
+/*
+ *	Copyright 2008 Isaac Truett.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ */
+package com.googlecode.simplegwt.client.ui.initialization;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+/**
+ * Event that occurs when an <code>Initializable</code> is initialized.
+ */
+public class InitializationEvent extends GwtEvent<InitializationEventHandler> {
+	private static final Type<InitializationEventHandler> TYPE = new Type<InitializationEventHandler>();
+
+	/**
+	 * Gets the event type associated with initialization events.
+	 * 
+	 * @return the handler type
+	 */
+	public static Type<InitializationEventHandler> getType() {
+		return TYPE;
+	}
+
+	/**
+	 * @see com.google.gwt.event.shared.GwtEvent#dispatch(com.google.gwt.event.shared.EventHandler)
+	 */
+	@Override
+	protected void dispatch(InitializationEventHandler handler) {
+		handler.onInitialize(this);
+	}
+
+	/**
+	 * @see com.google.gwt.event.shared.GwtEvent#getAssociatedType()
+	 */
+	@Override
+	protected Type<InitializationEventHandler> getAssociatedType() {
+		return TYPE;
+	}
+}
